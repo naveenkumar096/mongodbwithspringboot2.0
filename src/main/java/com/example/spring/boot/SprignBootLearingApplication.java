32d2159226b766f0d780d8bb85fdae6529a8dc5d
naveenkumar096 <naveenkumar096@users.noreply.github.com>
2019-08-23 14:09:01 +0530

    Update SprignBootLearingApplication.java

--- src/main/java/com/example/spring/boot/SprignBootLearingApplication.java
+++ src/main/java/com/example/spring/boot/SprignBootLearingApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @EnableMongoAuditing
 @EnableReactiveMongoRepositories
 @SpringBootApplication
-public class SprignBootLearingApplication {
+public class SprignBootLearingApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprignBootLearingApplication.class, args);
